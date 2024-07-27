7948bceca3847d9df5f55ca187c8f49638d09f1e
Denis Trofimliuk <smthwsmth@gmail.com>
2024-07-27 11:14:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw6compose/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.hw6compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hw6compose.ui.theme.HW6ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HW6ComposeTheme {
+            }
+        }
+    }
+}
